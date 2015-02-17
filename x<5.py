e0e9b37bc6811806dce7db71c47ec9ea14064754
JonTheSwimmer <j.zhang.1@alumni.ubc.ca>
2015-02-16 17:20:34 -0800

    Fixed

--- x<5.py
+++ x<5.py
@@ -1,5 +1,5 @@
 #First, let the user input a number
-x = input("Input a number")
+x = float(input("Input a number"))
 #The input function allows you to input a number when the program is running. The "" are used because it's a string (ie text).
 #Then, check if x < 5
 if x<5:
