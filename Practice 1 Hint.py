5f3a54a5117dbb7c7fc5126e55a621dd6cea480d
JonTheSwimmer <JonTheSwimmer@users.noreply.github.com>
2015-02-16 18:30:08 -0800

    Create Practice 1 Hint.py

---
+++ Practice 1 Hint.py
@@ -0,0 +1,8 @@
+x = int(input("Input the integer")
+lst = []
+for item in range(x):
+#range (x) generates a list from 0 to x-1, and then the "for" loop goes through each item in the list
+  a = #put in something that would help make a list from 1 to the number... 
+  #Think about what item is equal to, and how to get it to equal the number you want
+  lst.append(a)
+#Print out the list
